d7307d9b30134160b597ebde70675e56b4f72633
Sovello Hildebrand <sovellohpmgani@gmail.com>
2016-09-07 16:24:45 +0300

    remove and .pyc files and some modifications to views and settings

--- dcare/settings.py
+++ dcare/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'patient',
+    'geography',
 ]
 
 MIDDLEWARE_CLASSES = [
--- patient/views.py
+++ patient/views.py
@@ -1,6 +1,21 @@
+# Authentication
+from django.contrib.auth import authenticate, login
+
+#http requests
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-
-    return render(request, 'patient/index.html')
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return render(request, 'patient/index.html')
+        else:
+            message = "Your account is inactive! Contact your Administrator"
+            return render(request, 'dcare/index.html', {'error_message': message})
+    else:
+        message = "Invalid username and/or password"
+        return render(request, 'dcare/index.html', {'error_message': message})
