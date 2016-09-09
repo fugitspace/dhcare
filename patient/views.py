4ab1d28f54b419f82893c5f4f24ca1338df582db
Sovello Hildebrand <sovellohpmgani@gmail.com>
2016-09-09 08:54:27 +0300

    complete functionality to add and edit a patient

--- patient/views.py
+++ patient/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
 
 from person.forms import PersonForm, PersonContactForm, PersonDemographicForm
@@ -14,43 +14,34 @@ def home(request):
 
 
 def create_patient(request):
+    form_title = "Add New Patient"
     if request.method == 'POST':
         form = PersonForm(request.POST)
         if form.is_valid():
-            if len(request.POST['id']) == 0:
-                print("we are creating a new")
-                patient = Patient()
-            else:
-                patient = Patient.objects.get(pk=request.POST['id'])
+            patient = Patient()
             patient.surname = request.POST['surname']
             patient.firstname = request.POST['firstname']
             patient.othername = request.POST['othername']
-            print(request.POST['othername'])
-            print(request.POST['prefix'])
-            patient.prefix = Prefix.objects.get(id=request.POST['prefix'])
+            patient.prefix = Prefix.objects.get(pk = request.POST['prefix'])
             patient.save()
             return HttpResponseRedirect(reverse('patient:view_patient', args=(patient.id,)))
     else:
         form = PersonForm()
         
-    return render(request, 'patient/create_patient.html', {'form':form})    
+    return render(request, 'patient/create_patient.html', {'form':form, 'form_title':form_title})    
 
-def edit_patient(request, patient_id):    
-    
+def edit_patient(request, patient_id):
+    form_title = 'Edit Patient Information'
+    patient = get_object_or_404(Patient, pk=patient_id)
     if request.method == 'POST':
-        form = PersonForm(request.POST)
-        if form.is_valid():
-            patient = Patient.objects.get(pk=patient_id)
-            patient.surname = request.POST['surname']
-            patient.firstname = request.POST['firstname']
-            patient.othername = request.POST['othername']
-            patient.prefix = Prefix.objects.get(id=request.POST['prefix'])
-            patient.save()
+        form = PersonForm(request.POST, instance = patient)
+        if form.is_valid():            
+            form.save()
             return HttpResponseRedirect(reverse('patient:view_patient', args=(patient.id,)))
     else:
-        form = PersonForm()
+        form = PersonForm(instance = patient)
         
-    return render(request, 'patient/create_patient.html', {'form':form})
+    return render(request, 'patient/create_patient.html', {'form':form, 'form_title':form_title})
 
 def view_patient(request, patient_id):
     patient = Patient.objects.get(pk = patient_id)
