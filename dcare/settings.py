7c944827fb9b5f3293337067118c7581cd0b0c45
Sovello Hildebrand <sovellohpmgani@gmail.com>
2016-09-12 04:44:01 +0300

    set STATIC_ROOT path in settings

--- dcare/settings.py
+++ dcare/settings.py
@@ -136,8 +136,8 @@ TEMPLATES = [
 ]
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'dcare/static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
