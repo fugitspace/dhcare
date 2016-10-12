f6719aa0e6853151ffff6444e8fb12b7a5c31f2c
Sovello Hildebrand <sovellohpmgani@gmail.com>
2016-10-12 09:54:05 +0300

    move vitals from patient admin to encounter admin

--- encounter/admin.py
+++ encounter/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from encounter.models import Encounter, EncounterStatus
+from encounter.models import Encounter, EncounterStatus, PatientVitals, Vitals
 # Register your models here.
 
 class EncounterStatusAdmin(admin.ModelAdmin):
@@ -8,7 +8,16 @@ class EncounterStatusAdmin(admin.ModelAdmin):
     
 class EncounterAdmin(admin.ModelAdmin):
     list_display = ['patient', 'status', 'start_date', 'end_date']
-    
+
+class PatientVitalsAdmin(admin.ModelAdmin):
+    list_display = ['patient', 'measures']
+
+class VitalsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'active']
+
+
 
 admin.site.register(Encounter, EncounterAdmin)
 admin.site.register(EncounterStatus, EncounterStatusAdmin)
+admin.site.register(PatientVitals, PatientVitalsAdmin)
+admin.site.register(Vitals, VitalsAdmin)
