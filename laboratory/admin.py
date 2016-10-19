e9cda374f2ef75424d501cab3019fe74916e532d
Sovello Hildebrand <sovellohpmgani@gmail.com>
2016-10-19 12:53:11 +0300

    add models and admin for laboratory

--- laboratory/admin.py
+++ laboratory/admin.py
@@ -1,19 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
-from laboratory.models import DiagnosticServiceSection, Observation, ObservationGroup
+from laboratory.models import DiagnosticServiceSection, Observation, ObservationGroup, PatientLabRequest, PatientLabReport
 
 class DiagnosticServiceSectionAdmin(admin.ModelAdmin):
     list_display = ['name', 'code']
 
 class ObservationAdmin(admin.ModelAdmin):
-    list_display = ['name', 'code', 'category', 'unit', 'maximum', 'minimum']
+    list_display = ['name', 'code', 'category', 'group', 'unit', 'maximum', 'minimum']
     list_filter = ['category', 'group']
     search_fields = ['name', 'code', 'category', 'group']
 
 class ObservationGroupAdmin(admin.ModelAdmin):
     list_diplay = ['name', 'code']    
 
+class PatientLabRequestAdmin(admin.ModelAdmin):
+    list_display = ['requestor', 'request', 'request_notes', 'request_time', 'date_created', 'last_modified']
+class PatientLabReportAdmin(admin.ModelAdmin):
+    list_display = ['reporter', 'date_created']
+    
 admin.site.register(DiagnosticServiceSection, DiagnosticServiceSectionAdmin)
 admin.site.register(Observation, ObservationAdmin)
 admin.site.register(ObservationGroup, ObservationGroupAdmin)
+admin.site.register(PatientLabRequest, PatientLabRequestAdmin)
+admin.site.register(PatientLabReport, PatientLabReportAdmin)
--- laboratory/models.py
+++ laboratory/models.py
@@ -1,12 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 from encounter.models import Encounter
 from staff.models import Staff
 # Create your models here.
 class DiagnosticServiceSection(models.Model):
-    '''Diagnostic Service Station according to FHIR like dermatology, '''
+    '''Diagnostic Service Station according to FHIR like dermatology, 
+parasitology, haematology, biochemistry, etc '''
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=10, blank=True)
 
@@ -27,23 +29,27 @@ class Observation(models.Model):
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=100)    
     category = models.ForeignKey(DiagnosticServiceSection)
-    group = models.ForeignKey(ObservationGroup, blank=True)
-    unit = models.CharField(max_length=100, blank=True)
+    group = models.ForeignKey(ObservationGroup, blank=True, null=True)    
+    unit = models.CharField("Units", max_length=100, blank=True)
+    reference_value = models.CharField(max_length = 200, blank=True, null=True)
     maximum = models.CharField("Maximum Value", max_length=100, blank=True)
     minimum = models.CharField("Minimum Value", max_length=100, blank=True)
 
 
-class InvestigationStatus(models.Model):
-    pass
-
-class PatientLabInvestigation(models.Model):
+class PatientLabRequest(models.Model):
     encounter = models.ForeignKey(Encounter)
-    requestor = models.ForeignKey(Staff, related_name='medical_officer', null=True, blank=True)
+    requestor = models.ForeignKey(User, null=True, blank=True)
+    request = models.CharField(null=True, blank=True, max_length=1000)
     request_notes = models.TextField("Notes", blank=True, null=True)
-    request_time = models.DateTimeField(auto_now = True)
+    request_time = models.DateTimeField(auto_now = True)    
+    date_created = models.DateTimeField(auto_now_add = True)
+    last_modified = models.DateTimeField(auto_now = True)
+
+    
+class PatientLabReport(models.Model):
     reporter = models.ForeignKey(Staff, null=True, blank=True)
-    report_time = models.DateTimeField(auto_now=True)
-    report_notes = models.TextField("Notes", blank=True, null=True)
-    #status = models.ForeignKey(InvestigationStatus)    
+    report = models.TextField(null=True, blank=True)
+    report_time = models.DateTimeField(auto_now=True)    
+    report_notes = models.TextField("Notes", blank=True, null=True)    
     date_created = models.DateTimeField(auto_now_add = True)
     last_modified = models.DateTimeField(auto_now = True)
